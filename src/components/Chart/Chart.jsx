b6a686383172ff7b25105c2ad8169943bbcb2a4f
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-05-06 13:15:52 +0100

    Fix colours on bar chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -50,8 +50,8 @@ const Chart = ({ data: { confirmed, deaths, recovered }, country }) => {
             label: 'People',
             backgroundColor: [
               'rgba(0, 0, 255, 0.5)',
-              'rgba(255, 0, 0, 0.5)',
               'rgba(0, 255, 0, 0.5)',
+              'rgba(255, 0, 0, 0.5)',
             ],
             data:[confirmed.value, recovered.value, deaths.value]
           }]
